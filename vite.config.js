fb2026ad61346a39e90fa543742722a53ff00b97
RafiAthallah05 <mhmmdrafiath@students.undip.ac.id>
2025-12-01 14:19:19 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,50 @@
+// vite.config.js
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',   // WAJIB
-      strategies: 'generateSW', // WAJIB
-      manifestFilename: 'manifest.webmanifest',
-      
-      includeAssets: [
-        'favicon.ico',
-        'nba-logo.png',
-        'download.png'
-      ],
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png', 'LOGORN.png'],
+    
+    manifest: {
+      "name": "NBA Pedia 2025",
+      "short_name": "NBA",
+      "start_url": "https://ta-prak-ppb-nba-pedia-muhammad-rafi.vercel.app/",
+      "scope": "https://ta-prak-ppb-nba-pedia-muhammad-rafi.vercel.app/",
+      "display": "standalone",
+      "background_color": "#ffffff",
+      "theme_color": "#0f4c81",
+      "icons": [
+        {
+          "src": "/download.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "/download.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        }
+      ]
+    },
 
-      manifest: {
-        name: 'NBA Pedia 2025',
-        short_name: 'NBA',
-        start_url: '/',
-        scope: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#0f4c81',
-        icons: [
-          { src: '/nba-logo.png', sizes: '192x192', type: 'image/png' },
-          { src: '/download.png', sizes: '512x512', type: 'image/png' }
-        ]
-      },
+    workbox: {
+      globPatterns: ['/*.{js,css,html,svg,png,ico,jpg,jpeg}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+      // TAMBAHKAN BARIS INI: Izinkan file hingga 5 MB
+      maximumFileSizeToCacheInBytes: 5000000, 
+    },
 
-      workbox: {
-        navigateFallback: '/offline.html',
-        runtimeCaching: [
-          {
-            urlPattern: ({ url }) => url.origin === self.location.origin,
-            handler: 'CacheFirst',
-          }
-        ]
-      }
-    })
-  ]
+    devOptions: {
+      enabled: true,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
 })
